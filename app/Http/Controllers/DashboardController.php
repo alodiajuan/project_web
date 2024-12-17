2408843af1b4f2b3d8c026fadfffc43de5bacbe5
mallexibra <mallexibra@gmail.com>
2024-12-17 22:24:21 +0700

    feat: dashboard users

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Compensation;
+use App\Models\Task;
+use App\Models\TaskRequest;
+use App\Models\TaskSubmission;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
@@ -15,6 +21,55 @@ class DashboardController extends Controller
 
         $activeMenu = 'dashboard';
 
-        return view('dashboard.index', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+        $user = Auth::user();
+
+        if ($user->role == "admin") {
+            $userCount = User::count();
+            $requestCount = TaskRequest::whereHas('task', function ($query) use ($user) {
+                $query->where('id_dosen', $user->id);
+            })
+                ->whereNull('status')
+                ->count();
+            $compensationCount = Compensation::count();
+            $dataDashboard = [
+                "primary" => [$userCount, "Users"],
+                "second" => [$requestCount, "Requests"],
+                "three" => [$compensationCount, "Compensations"]
+            ];
+        } else if ($user->role == "dosen" || $user->role == "tendik") {
+            $taskCount = Task::where('id_dosen', $user->id)->count();
+            $requestCount = TaskRequest::whereHas('task', function ($query) use ($user) {
+                $query->where('id_dosen', $user->id);
+            })
+                ->whereNull('status')
+                ->count();
+
+            $submissionCount = TaskSubmission::whereHas('task', function ($query) use ($user) {
+                $query->where('id_dosen', $user->id);
+            })
+                ->count();
+            $dataDashboard = [
+                "primary" => [$taskCount, "Tasks"],
+                "second" => [$requestCount, "Requests"],
+                "three" => [$submissionCount, "Submissions"]
+            ];
+        } else {
+            $taskCount = Task::whereDoesntHave('taskSubmissions', function ($query) use ($user) {
+                $query->where('id_mahasiswa', $user->id);
+            })->count();
+            $requestCount = TaskRequest::where('id_mahasiswa', $user->id)
+                ->whereNull('status')
+                ->count();
+            $totalBobot = Compensation::where('id_mahasiswa', $user->id)
+                ->join('task', 'compensation.id_task', '=', 'task.id') // Bergabung dengan tabel task
+                ->sum('task.bobot');
+            $dataDashboard = [
+                "primary" => [$taskCount, "Tasks"],
+                "second" => [$requestCount, "Requests"],
+                "three" => [$totalBobot, "Compensations"]
+            ];
+        }
+
+        return view('dashboard.index', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu, "data" => $dataDashboard]);
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,13 +1,39 @@
 @extends('layouts.template')
 
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            <h3 class="card-title"> Halo semuanya! Selamat datang! </h3>
-            <div class="card-tools"></div>
+    <div class="row">
+        <div class="col-lg-4 col-6">
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>{{ $data['primary'][0] }}</h3>
+                    <p>Total {{ $data['primary'][1] }}</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-bag"></i>
+                </div>
+            </div>
         </div>
-        <div class="card-body">
-            Selamat datang semua a, ini adalah halaman utama dari aplikasi ini. Semoga paham dengan aplikasi ini ya!
+        <div class="col-lg-4 col-6">
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>{{ $data['second'][0] }}</h3>
+                    <p>Total {{ $data['second'][1] }}</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-6">
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>{{ $data['three'][0] }}</h3>
+                    <p>Total {{ $data['three'][1] }}</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                </div>
+            </div>
         </div>
     </div>
 @endsection

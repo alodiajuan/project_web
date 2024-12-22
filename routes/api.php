e8201e133749f088a23aba52f5824d1f77419f12
Hanif <nurhanif2312@gmail.com>
2024-12-22 10:35:06 +0700

    feat: complete all features/api for auth, task, taskrequest, task submission, dashboard, compensation, competence, type task, and prodi

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Api\TaskRequestController;
 use App\Http\Controllers\Api\DashboardController;
 use App\Http\Controllers\Api\CompensationController;
 use App\Http\Controllers\Api\ProdiController;
-use App\Http\Controllers\Api\TypeTaskController;    
+use App\Http\Controllers\Api\TypeTaskController;
 use App\Http\Controllers\Api\CompetenceController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,50 +27,54 @@ Route::prefix('v1')->group(function () {
 
         // Task Routes
         Route::prefix('tasks')->group(function () {
-            Route::post('/store', [TaskController::class, 'store'])
-                ->middleware('role:admin,dosen,tendik');
+            Route::middleware('role:admin,dosen,tendik')->group(function () {
+                Route::post('/store', [TaskController::class, 'store']);
+                Route::post('/update', [TaskController::class, 'update']);
+                Route::get('/sdm', [TaskController::class, 'getTasksForSdm']);
+                Route::get('/{id}', [TaskController::class, 'getTaskById']);
+            });
             Route::get('/student', [TaskController::class, 'getTasksForStudent'])
                 ->middleware('role:mahasiswa');
-            Route::get('/sdm', [TaskController::class, 'getTasksForSdm'])
-                ->middleware('role:admin,dosen,tendik');
-            Route::post('/update', [TaskController::class, 'update'])
-                ->middleware('role:admin,dosen,tendik');
-            Route::get('/{id}', [TaskController::class, 'getTaskById'])
-                ->middleware('role:admin,dosen,tendik,mahasiswa');
         });
 
         // Task Request Routes
-        Route::prefix('task-requests')->group(function () {
-            Route::post('/', [TaskRequestController::class, 'submitTaskRequest'])
-                ->middleware('role:admin,dosen,tendik');
-            Route::get('/', [TaskRequestController::class, 'getAllTaskRequests'])
-                ->middleware('role:admin,dosen,tendik');
+        Route::prefix('task-requests')->middleware('role:admin,dosen,tendik')->group(function () {
+            Route::post('/', [TaskRequestController::class, 'submitTaskRequest']);
+            Route::get('/', [TaskRequestController::class, 'getAllTaskRequests']);
         });
 
         // Task Submission Routes
         Route::prefix('submissions')->group(function () {
-            Route::get('/task/{id}', [TaskSubmissionController::class, 'getSubmissionsByTaskId']);
-            Route::get('/sdm', [TaskSubmissionController::class, 'getSubmissionsForSdm']);
-            Route::post('/', [TaskSubmissionController::class, 'store']);
+            Route::get('/task/{id}', [TaskSubmissionController::class, 'getSubmissionsByTaskId'])
+                ->middleware('role:mahasiswa');
+            Route::get('/sdm', [TaskSubmissionController::class, 'getSubmissionsForSdm'])
+                ->middleware('role:admin,dosen,tendik');
+            Route::post('/', [TaskSubmissionController::class, 'store'])
+                ->middleware('role:mahasiswa');
             Route::post('/{id}', [TaskSubmissionController::class, 'reviewSubmission'])
                 ->middleware('role:admin,dosen,tendik');
-            Route::get('/{id}', [TaskSubmissionController::class, 'getSubmissionById']);
-            Route::get('/task-request/{id}', [TaskSubmissionController::class, 'requestTask']);
+            Route::get('/{id}', [TaskSubmissionController::class, 'getSubmissionById'])
+                ->middleware('role:admin,dosen,tendik,mahasiswa');
+            Route::get('/task-request/{id}', [TaskSubmissionController::class, 'requestTask'])
+                ->middleware('role:admin,dosen,tendik');
         });
 
         // Compensation Routes
-        Route::prefix('compensations')->group(function () {
+        Route::prefix('compensations')->middleware('role:admin,dosen,tendik')->group(function () {
             Route::get('/', [CompensationController::class, 'index']);
             Route::get('/{id}', [CompensationController::class, 'show']);
         });
 
         // Competence Routes
-        Route::get('/competences', [CompetenceController::class, 'index']);
+        Route::get('/competences', [CompetenceController::class, 'index'])
+            ->middleware('role:admin,dosen,tendik');
 
         // Prodi Routes
-        Route::get('/prodi', [ProdiController::class, 'index']);
+        Route::get('/prodi', [ProdiController::class, 'index'])
+            ->middleware('role:admin,dosen,tendik');
 
         // Type Task Routes
-        Route::get('/type-tasks', [TypeTaskController::class, 'index']);
+        Route::get('/type-tasks', [TypeTaskController::class, 'index'])
+            ->middleware('role:admin,dosen,tendik');
     });
 });
